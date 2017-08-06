a29ba97dc72d896d2ca698996088a2c7811cae39
karthikkvhathwar <30752746+karthikkvhathwar@users.noreply.github.com>
2017-08-06 19:17:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,16 @@
 console.log('Loaded!');
+
+var element = document.getElemntById("main-text");
+
+element.innerHTML = 'New Value';
+
+var img = element.getElelementById("madi");
+var marginLeft = 0;
+function moveRight() {
+    marginLeft = marginLeft + 1;
+    img.style.marginLeft = marginLeft + 'px';
+}
+
+img.onClick = function () {
+    var interval = setInterval(moveRight,50);
+}
