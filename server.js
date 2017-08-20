9c1f3a4848277ea6cef0e1a0f3f94199de74c5ef
karthikkvhathwar <30752746+karthikkvhathwar@users.noreply.github.com>
2017-08-20 23:10:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,9 +3,28 @@ var morgan = require('morgan');
 var path = require('path');
 
 var app = express();
+var Pool = require('pg').Pool;
+var config = {
+    user:'karthikhathwar28',
+    database:'karthikhathwar28',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password:process.env.DB_PASSWORD
+};
 app.use(morgan('combined'));
 
 var names = [];
+app.get('/test-db', function (req, res) {
+  pool.query('SELECT * FROM test',function(err,result) {
+      if(err) {
+          res.status(500).send(err.toString());
+      } else {
+          res.send(JSON.stringify(result));
+      }
+  });
+});
+
+var pool = new Pool(config);
 app.get('/submit-name', function (req, res) {
    var name = req.query.name;
    
