8b30ccdf535eabce72e894ad4eec31976e512c81
karthikkvhathwar <30752746+karthikkvhathwar@users.noreply.github.com>
2017-09-10 21:24:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -82,6 +82,16 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+function hash(input,salt) {
+    var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    return hashed.toString('hex');
+}
+
+app.get("/hash/:input",function(req,res){
+    var hashedString = hash(req.params.input,'this-is-some-random-String');
+    res.send(hashedString);
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
