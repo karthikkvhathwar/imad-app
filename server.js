ffa491763c31d362808e82fde96ed7201b07dabb
karthikkvhathwar <30752746+karthikkvhathwar@users.noreply.github.com>
2017-09-10 21:37:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var crypto = require('crypto');
 var app = express();
 var Pool = require('pg').Pool;
 var config = {
